a3186ec154b71fa9cc1c8b683ac20d842c778261
malk <malk1405@mail.ru>
2019-11-20 23:14:13 +0300

    Updates

---
+++ precache-manifest.a11152f9570cbfc713b221165265d386.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4affe6d7c70df30c4869025c1d0734b6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "796a38760d14d22ed159",
+    "url": "./static/css/main.5460c4de.chunk.css"
+  },
+  {
+    "revision": "83bb83e6615d96af8981",
+    "url": "./static/js/2.e51aec1c.chunk.js"
+  },
+  {
+    "revision": "796a38760d14d22ed159",
+    "url": "./static/js/main.b99f38e0.chunk.js"
+  },
+  {
+    "revision": "381fff3624d966f7e76c",
+    "url": "./static/js/runtime-main.ca8cda72.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.adaf6bc80588bcb9264e787d83c9e092.js"
+  "./precache-manifest.a11152f9570cbfc713b221165265d386.js"
 );
 
 self.addEventListener('message', (event) => {
