009ecef129436daaa50d1d87277edc9cf05cc05c
malk <malk1405@mail.ru>
2019-11-22 15:26:34 +0300

    динамически сгенерил планы

--- src/components/Home/Hosting/Hosting.js
+++ src/components/Home/Hosting/Hosting.js
@@ -1,4 +1,6 @@
 import React from "react";
+import plans from "./Plans";
+import { Button } from "../../Button/Button";
 
 import "./hosting.css";
 
@@ -14,50 +16,17 @@ function Hosting() {
       </section>
       <section>
         <ul className="hosting__plans">
-          <li className="hosting__plan">
-            <div className="plan__logo"></div>
-            <h3 className="plan__title">Share Hosting</h3>
-            <p className="plan__description">
-              Easy drag and drop fully customizable mobile friendly
-            </p>
-            <p className="plan__price">
-              Start from <span>$2.5/m</span>
-            </p>
-            <button className="btn">Start now</button>
-          </li>
-          <li className="hosting__plan">
-            <div className="plan__logo"></div>
-            <h3 className="plan__title">VPS Hosting</h3>
-            <p className="plan__description">
-              Easy drag and drop fully customizable mobile friendly
-            </p>
-            <p className="plan__price">
-              Start from <span>$2.5/m</span>
-            </p>
-            <button className="btn">Start now</button>
-          </li>
-          <li className="hosting__plan">
-            <div className="plan__logo"></div>
-            <h3 className="plan__title">Wordpress Hosting</h3>
-            <p className="plan__description">
-              Easy drag and drop fully customizable mobile friendly
-            </p>
-            <p className="plan__price">
-              Start from <span>$2.5/m</span>
-            </p>
-            <button className="btn">Start now</button>
-          </li>
-          <li className="hosting__plan">
-            <div className="plan__logo"></div>
-            <h3 className="plan__title">Dedicated Hosting</h3>
-            <p className="plan__description">
-              Easy drag and drop fully customizable mobile friendly
-            </p>
-            <p className="plan__price">
-              Start from <span>$2.5/m</span>
-            </p>
-            <button className="btn">Start now</button>
-          </li>
+          {plans.map(({ title, description, price }) => (
+            <li className="hosting__plan">
+              <div className="plan__logo"></div>
+              <h3 className="plan__title">{title}</h3>
+              <p className="plan__description">{description}</p>
+              <p className="plan__price">
+                Start from <span>${price}/m</span>
+              </p>
+              <Button className="btn">Start now</Button>
+            </li>
+          ))}
         </ul>
       </section>
     </section>
---
+++ src/components/Home/Hosting/Plans.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    title: "Share Hosting",
+    description: "Easy drag and drop fully customizable mobile friendly",
+    price: 2.5
+  },
+  {
+    title: "VPS Hosting",
+    description: "Easy drag and drop fully customizable mobile friendly",
+    price: 1.5
+  },
+  {
+    title: "Wordpress Hosting",
+    description: "Easy drag and drop fully customizable mobile friendly",
+    price: 5.5
+  },
+  {
+    title: "Dedicated Hosting",
+    description: "Easy drag and drop fully customizable mobile friendly",
+    price: 3.5
+  }
+];
