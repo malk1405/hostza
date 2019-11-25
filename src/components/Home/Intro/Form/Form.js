080233dacc175f417fe0a327a8f764823b6997e2
malk <malk1405@mail.ru>
2019-11-25 23:00:25 +0300

    Поправил стили

--- src/components/Home/Intro/Form/Form.js
+++ src/components/Home/Intro/Form/Form.js
@@ -1,16 +1,16 @@
 import styled from "styled-components";
+import media from "../../../../utils/media";
 
 const Form = styled.form`
   border-radius: 5px;
   display: flex;
   overflow: hidden;
   width: 90%;
-  max-width: 1000px;
 
   & input,
   & button {
     font-family: inherit;
-    font-size: 0.9rem;
+    font-size: 1rem;
   }
 
   & input {
@@ -23,9 +23,14 @@ const Form = styled.form`
     background-color: #670080;
     border: none;
     color: white;
+    flex-basis: 30%;
     flex-shrink: 0;
     padding: 1rem 2rem;
   }
+
+  ${media.xl`
+    width: 50%;
+  `}
 `;
 
 export default Form;
